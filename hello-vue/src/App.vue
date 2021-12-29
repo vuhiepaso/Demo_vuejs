3253e1610f173ea2cd15dca7df3e63d78485c200
vuhiepaso <vuhiepomg@gmail.com>
2021-12-29 17:36:24 +0700

    create

---
+++ hello-vue/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <header id="header">
+    <h1> hiep</h1>
+    </header>
+    <HelloWorld msg="hiep"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+#header{
+  background-color: aqua;
+}
+</style>
